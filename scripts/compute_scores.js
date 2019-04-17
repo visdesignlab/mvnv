bd4e79c7ba7a6cd4c615a258b262e83fdd026bb2
cnobre <carolinanobre@gmail.com>
2019-04-17 15:33:57 -0600

    added mini panels to each technique. fully interactive from the main panel, not yet from the mini panel

--- scripts/compute_scores.js
+++ scripts/compute_scores.js
@@ -1,3 +1,184 @@
+function create_panel(allScores){
+
+  let groups = d3.select('#wizard_panel').selectAll('div').data(Object.keys(optionsObject));
+
+let groupsEnter = groups.enter().append('div').attr('class','dataDiv');
+
+//Add Header
+let h4 = groupsEnter.append('h4');
+
+h4.append('span').attr('id','header');
+h4.append('span').attr('id','icon')
+.append('i');
+
+let switchEl = h4.append('span').attr('class','field is-pulled-right');
+
+switchEl.append('input')
+.attr('type','checkbox')
+.attr('class','switch is-small')
+
+switchEl.append('label')
+
+//Add options
+groupsEnter.append('div').attr('class','tabs is-fullwidth is-toggle'); 
+
+let ul = groupsEnter.select('.tabs')
+.append('ul');
+
+groups = groupsEnter.merge(groups);
+
+//Set data dependent attributes
+
+groups
+.attr('class',d=>'dataDiv data_' + d);
+
+groups.select('.switch')
+.attr('id',d=>'switch_'+d)
+
+groups.select('label')
+.attr('for',d=>'switch_'+d)
+
+groups.select('#header')
+.text(d=>optionsObject[d].label);
+
+groups.select('#icon')
+.attr('class','icon tooltip')
+.attr('data-tooltip','tooltip info');
+
+groups.select('i')
+.attr('class','fas fa-question-circle has-text-grey');
+
+
+let li = groups.select('ul').selectAll('li').data((d)=>optionsObject[d].options.map(
+option=>{return {category:d,option}}));
+
+let liEnter = li.enter().append('li');
+
+liEnter.append('a').append('span');
+
+li = liEnter.merge(li);
+
+li.attr('class',d=> d.category +  ' ' + d.option);
+
+li.select('span').text(d=>d.option);
+
+
+li.on('click',function(d){
+
+  compute_scores(allScores);
+ 
+let tabGroup = d3.select((this.parentNode).parentNode);
+let category = tabGroup.data()[0];
+
+// console.log('clicked', d);
+let isSelected = d3.select(this).classed('is-active');
+
+if (category !== 'structure'){
+  tabGroup.selectAll('li').classed('is-active',false);
+}
+d3.select(this).classed('is-active',!isSelected);
+
+//if no elements are selected, set toggle to false;
+selectedTabs = tabGroup.selectAll('.is-active');
+
+d3.select('#switch_' + category)
+.property('checked',selectedTabs.empty()? false : true)
+
+//highlight all mini panel buttons for active tabs. 
+selectedTabs = d3.select('#wizard_panel').selectAll('.is-active').each(function(tab){
+  let currentClass = d3.select(this).attr('class').replace('is-active','').trim();
+  let miniButtons = d3.selectAll('.button').filter(function(b){
+      return d3.select(this).attr('class').includes(currentClass);
+   }).classed('is-focused',true);
+})
+
+
+
+});
+
+d3.selectAll(".switch").on("change", function(d){
+
+let tabGroup = d3.select(((this.parentNode).parentNode).parentNode).select('.tabs');
+let category = tabGroup.data()[0];
+
+d3.select(this).property('checked',false); //for now disable turning the toggle on;
+
+// let isOn = d3.select(this).property('checked');
+
+// console.log('d is ',d, 'isOn is', isOn);
+
+// let tabGroup = d3.select(((this.parentNode).parentNode).parentNode).select('.tabs');
+// let tabGroup = d3.select('.data_' + d)
+
+// console.log('data1',tabGroup.selectAll('li').data())
+
+tabGroup.selectAll('li')
+.classed('is-active',false);
+
+// if (isOn){
+//   console.log('datum',tabGroup.select('li').data())
+
+//   // tabGroup.select('li')
+//   // .classed('is-active',true);
+
+//   console.log('datum for li',tabGroup.select('li').data())
+//   console.log('allData',tabGroup.selectAll('li').data())
+// }
+
+compute_scores(allScores);
+});
+
+compute_scores(allScores);
+}
+
+function create_mini_panel(techniques,allScores){
+
+  let score2class = {
+    "1": "score-one",
+    "2": "score-two",
+    "3": "score-three",
+    "0": "score-zero"
+  };
+
+  let cards = d3.selectAll('.techniqueCard').data(techniques.map(t=>t[0]));
+
+
+let groups = cards.select('#mini_wizard_panel').selectAll('div').data(d=>Object.keys(optionsObject).map(k=>{return {'technique':d,'dimension':k}}));
+
+let groupsEnter = groups.enter().append('div');
+
+//Add Header
+let h4 = groupsEnter.append('h6');
+
+h4.append('span').attr('id','header');
+
+groups = groupsEnter.merge(groups);
+
+//Set data dependent attributes
+
+
+groups.select('#header')
+.text(d=>optionsObject[d.dimension].shortLabel ? optionsObject[d.dimension].shortLabel : optionsObject[d.dimension].label);
+
+
+let li = groups.selectAll('a').data((d)=>optionsObject[d.dimension].options.map(
+option=>{return {category:d.dimension,option,technique:d.technique}}));
+
+let liEnter = li.enter().append('a');
+
+liEnter.append('a').append('span');
+
+li = liEnter.merge(li);
+
+li.attr('class',d=>{
+  let score = allScores[d.technique][d.category][d.option];
+  return d.category +  ' ' + d.option + ' ' + score2class[score] + ' button tooltip';
+})
+.attr('data-tooltip',d=>d.option);
+
+li.text('');
+}
+
 function compute_scores(allScores){
 
   d3.select('#recommendations').style('visibility','visible');
@@ -19,8 +200,6 @@ function compute_scores(allScores){
     allScores[technique].twos=[];
     allScores[technique].ones=[];
     allScores[technique].zeros=[];
-
-
 
     activeOptions.map(option=>{
       let score = allScores[technique][option.category][option.option];
@@ -51,7 +230,7 @@ function compute_scores(allScores){
     }
   })
 
-  console.log('allScores_computed',scoreArray)
+  // console.log('allScores_computed',scoreArray)
 
   render_techniques(scoreArray,allScores);
 }
@@ -60,10 +239,7 @@ function render_techniques(techniques,info) {
 
    let cards = d3.selectAll('.techniqueCard').data(techniques);
 
-  //  cards.select('h4').select('span').text(d=>info[d[0]].title)
-
-
-   cards.select('h4').select('span').html(d=>'<a href="' + info[d[0]].baseUrl + info[d[0]].url + '">' + info[d[0]].title + '</a>');
+   cards.select('.rec').select('h4').select('span').html(d=>'<a href="' + info[d[0]].baseUrl + info[d[0]].url + '">' + info[d[0]].title + '</a>');
 
    cards.select('.totalScore')
    .classed('score-three',d=>d[1]>=2.5)
@@ -80,15 +256,12 @@ function render_techniques(techniques,info) {
 
   let three = cards.select('.threeTags').selectAll('.tag').data(d=>info[d[0]].threes);
 
-  let threeEnter = three.enter().append('span');
+  let threeEnter = three.enter().append('span').attr('class','tag score-three');
 
   three.exit().remove();
 
   three = threeEnter.merge(three);
 
-  three
-  .classed('tag',true)
-  .classed('is-success', true);
   three.text(d=>{
     let string; 
     if (d[0].includes('node_attr')){
@@ -173,56 +346,8 @@ function render_techniques(techniques,info) {
    //only show this for techniques that actually have a 0 score.
    cards.select('.scoreZero')
    .style('display',d=> info[d[0]].bad.length === 0 ?  'none' : 'block');
-  //  let strengthTags = cards.select('.strengthTags').selectAll('tag').data();
-
-
-  //  <span class="tag is-success  ">Large <br></span>
-
-
-   
-
-   //description info[d[0]].description
-
-   //strengths  info[d[0]].strengths
-
-   //weaknesses info[d[0]].weaknesses
-
-   //limitations info[d[0]].limitations
-
-   
-  console.log('info ', info)
-
-
-  //  let cardsEnter = cards.enter().append('div').attr('class','box techniqueCard');
-
-  //  let article = cardsEnter.append('article').attr('class','media');
-
-  //  article 
-  //  .append('div').attr('class','media-left')
-  //  .append('figure').attr('class','image is-96x96')
-  //  .append('img');
-  //  //data driven src attribute
-
-  //  let content = article
-  //  .append('div').attr('class','media-content')
-  //  .append('div').attr('class','content');
-
-
-  //  //card header
-  // let header = content.append('h4');
-  // header.append('span'); //data driven header;
-  // header.append('div')
   
-  // //info icon
-  // content.append('p')
-  // .append('span')
-  // .attr('class','icon has-text-info tooltip')
-  // .attr('data-tooltip','Technique Description');
-
-  // content.append('p')
-  // .append('span')
-  // .attr('class','description') //data driven text goes here;
-
+   create_mini_panel(techniques,info);
 
 
 }
